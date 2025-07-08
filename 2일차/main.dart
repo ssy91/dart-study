cf590f504b2c626ccc8938d0416e51ab1471af75
ssy91 <smi1eday@naver.com>
2025-07-08 13:34:37 +0900

    퀴즈풀이2 구구단 1단 만들기

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,15 +1,19 @@
 void main() {
-  String a = 'banana';
-  
-  if(a == '1') {
-     print("object");
-     
-  } else if (a == "apple"){
-    print("apple");
-  } else{print("no case");
-  }
-    
-    
-  }
- 
- 
+// 구구단 1단 만들기
+
+ var dan = 1;
+ var count = 1;
+
+ for(count = 1; count <=9; count++) {
+print("$dan x $count = ${dan * count}");
+ }
+
+// 선생님 답
+var dan1= 1;
+var count1 = 1;
+var result = 0;
+
+for (count1 = 1; count1 <= 9; count1++) {
+print("$dan x $count1 = $result");
+}
+}
