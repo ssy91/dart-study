d1b7b38b9a816e5f4dd5355031c9fd00ee45bc8c
ssy91 <smi1eday@naver.com>
2025-07-09 09:34:12 +0900

    3일차

--- 2일차/2번문제.dart
+++ 2일차/2번문제.dart
@@ -1,12 +1,13 @@
 void main() {
   double m = 1.76;
   int kg = 63;
-String status = '';
+  String status = '';
   double getSum(var m, var kg) => kg / (m * m);
 
-  var score = getSum(m,kg);
-  if(score >= 18.4) {
-    status = "과체중";
-  } else if (score <= 18.5 &&score <= 22.9) {
+  var score = getSum(m, kg);
+  if (score >= 18.4) {
+    status = "저체중";
+  } else if (score <= 18.5 && score <= 22.9) {
     status = "정상";
+  }
 }
